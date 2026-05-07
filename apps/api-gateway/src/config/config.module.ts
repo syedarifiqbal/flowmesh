2a9721f5c894208d45958cba7b7d5c0868354a48
Syed Arif Iqbal <arifiqbal@outlook.com>
2026-05-07 22:14:52 +0300

    chore(docker): add analytics service to compose and infra config

--- apps/api-gateway/src/config/config.module.ts
+++ apps/api-gateway/src/config/config.module.ts
@@ -16,6 +16,7 @@ import Joi from 'joi'
         CONFIG_SERVICE_URL: Joi.string().required(),
         AUTH_SERVICE_URL: Joi.string().required(),
         PIPELINE_SERVICE_URL: Joi.string().required(),
+        ANALYTICS_SERVICE_URL: Joi.string().required(),
         RATE_LIMIT_INGEST_RPM: Joi.number().default(1000),
         RATE_LIMIT_MGMT_RPM: Joi.number().default(100),
       }),
